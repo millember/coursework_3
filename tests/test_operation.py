3c760873c94142b5614ac7121cf15f82a62c0059
Niyaz Kashapov <cashap.niyaz20@gmail.com>
2024-05-26 22:23:24 +0500

    finish test operation

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -3,22 +3,6 @@ from src.operation import Operation
 
 
 @pytest.fixture
-def example_data():
-    return Operation(
-        id=441945886,
-        state="EXECUTED",
-        date="2019-08-26T10:50:58.294041",
-        operationAmount={
-            "amount": "31957.58",
-            "currency": {"name": "руб.", "code": "RUB"},
-        },
-        description="Перевод организации",
-        where="Maestro 1596837868705199",
-        to="Счет 64686473678894779589",
-    )
-
-
-@pytest.fixture
 def list_of_examples():
     return [
         {
@@ -44,18 +28,6 @@ def list_of_examples():
             "description": "Перевод организации",
             "where": "MasterCard 7158300734726758",
             "to": "Счет 35383033474447895560",
-        },
-        {
-            "id": 939719570,
-            "state": "EXECUTED",
-            "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {
-                "amount": "9824.07",
-                "currency": {"name": "USD", "code": "USD"},
-            },
-            "description": "Перевод организации",
-            "where": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702",
         },
     ]
 
@@ -87,55 +59,15 @@ def list_of_operations():
             to="Счет 35383033474447895560",
             where="MasterCard 7158300734726758",
         ),
-        Operation(
-            id=939719570,
-            state="EXECUTED",
-            date="2018-06-30T02:08:58.425572",
-            operationAmount={
-                "amount": "9824.07",
-                "currency": {"name": "USD", "code": "USD"},
-            },
-            description="Перевод организации",
-            to="Счет 11776614605963066702",
-            where="Счет 75106830613657916952",
-        ),
     ]
-
-
-def test_field_access(example_data):
-    operation = example_data
-    assert operation.id == 441945886
-    assert operation.state == "EXECUTED"
-    assert operation.date == "019-08-26T10:50:58.294041"
-    assert operation.operationAmount == {
-        "amount": "31957.58",
-        "currency": {"name": "руб.", "code": "RUB"},
-    }
-    assert operation.description == "Перевод организации"
-    assert operation.where == "Maestro 1596837868705199"
-    assert operation.to == "Счет 64686473678894779589"
 
 
 def test_from_list(list_of_examples, list_of_operations):
     assert Operation.from_dict(list_of_examples) == list_of_operations
 
 
-def test_equality(example_data):
-    operation_1 = example_data
-    operation_2 = example_data
-    assert operation_1 == operation_2
-
-
 def test_inequality(list_of_examples):
-    operation_1 = Operation(**list_of_examples[0])
-    operation_2 = Operation(**list_of_examples[1])
-    assert operation_1 != operation_2
-
-
-def test_identity(example_data):
-    operation_1 = example_data
-    operation_2 = example_data
-    assert operation_1 is operation_2
+    assert Operation(**list_of_examples[0]) != Operation(**list_of_examples[1])
 
 
 def test_date(list_of_examples):
